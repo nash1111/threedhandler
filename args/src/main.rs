4db8850563d549a02535d8257c7f833f77bf4d42
nash1111 <ken1251213@gmail.com>
2022-12-03 18:49:22 +0900

    Add argparser

--- args/src/main.rs
+++ args/src/main.rs
@@ -1,3 +1,31 @@
+#![allow(unused)]
+
+use clap::Parser;
+use std::io::BufReader;
+use std::fs::File;
+use std::io::BufRead;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    let content = File::open(&args.path).expect("could not read file");
+    let mut reader = BufReader::new(content);
+    let mut line = String::new();
+
+    for line in reader.lines() {
+        if line.as_ref().unwrap().contains(&args.pattern) {
+            println!("{}", line.unwrap());
+        }
+    }
+    println!("path = {:?}", args.path);
+    println!("pattern = {:?}", args.pattern);
 }
